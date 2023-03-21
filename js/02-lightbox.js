e4ae873af64dc9f85ea405b7d07eeea57caa7100
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-03-21 13:18:57 +0100

    markup task two, added librady

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems.map((item) =>
+
+`<li class="gallery__item">
+<a class="gallery__link" href=${item.original}>
+   <img class="gallery__image" src=${item.preview} alt=${item.description} />
+</a>
+</li>`
+).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+galleryRef.addEventListener('click', e => {
+    e.preventDefault();
+ const openGalleryImage = new SimpleLightbox('.gallery a', { });
+ 
+});
+
 
 console.log(galleryItems);
